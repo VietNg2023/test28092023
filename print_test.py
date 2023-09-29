95973476a721174ae492cd00d93807a7e96acd53
Viet Nguyen <minhviet.turku@gmail.com>
2023-09-29 10:39:19 +0300

    finish-test-gutter

---
+++ print_test.py
@@ -0,0 +1,6 @@
+print("we makes some changes")
+print("we have some new test")
+print("why not show gutter- it start again")
+###good idea-> should commit and we see guiter
+print("why not show gutter")
+print("new test gutter")
